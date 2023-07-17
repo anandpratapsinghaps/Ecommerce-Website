c19421a037c4f7ef07f21b790790e52caa09072c
Anand Pratap Singh <anandpratap.anju123@gmail.com>
2023-07-17 22:42:33 +0530

    last commit

--- E-commerce/app.js
+++ E-commerce/app.js
@@ -8,3 +8,25 @@ function menutoggle(){
         Menuitems.style.maxHeight="0px";
     }
 }
+
+// for product
+
+var ProductImg=document.getElementById("ProductImg");
+var smallimg=document.getElementsByClassName("smallimg");
+
+    smallimg[0].onclick=function()
+    {
+        ProductImg.src=smallimg[0].src;
+    }
+    smallimg[1].onclick = function()
+    {
+        ProductImg.src=smallimg[1].src;
+    }
+    smallimg[2].onclick=function()
+    {
+        ProductImg.src=smallimg[2].src;
+    }
+    smallimg[3].onclick=function()
+    {
+        ProductImg.src=smallimg[3].src;
+    }
