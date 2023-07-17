b97e41f643d73be21492b66a48dd32d13496b70b
Anand Pratap Singh <anandpratap.anju123@gmail.com>
2023-07-17 19:53:37 +0530

    last commit

---
+++ E-commerce/app.js
@@ -0,0 +1,10 @@
+var Menuitems= document.getElementById("Menuitems");
+Menuitems.style.maxHeight="0px"
+function menutoggle(){
+    if(Menuitems.style.maxHeight=="0px"){
+        Menuitems.style.maxHeight="200px";
+    }
+    else{
+        Menuitems.style.maxHeight="0px";
+    }
+}
