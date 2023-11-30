2f1bbc41594a05a56416e65504b4cd02779a4b4e
Anand Singh <anandpratap.anju123@gmail.com>
2023-11-30 23:29:37 +0530

    last commit

--- RE_VIT/app.js
+++ RE_VIT/app.js
@@ -8,29 +8,6 @@ function menutoggle(){
         Menuitems.style.maxHeight="0px";
     }
 }
-
-// for product
-
-var ProductImg=document.getElementById("ProductImg");
-var smallimg=document.getElementsByClassName("smallimg");
-
-    smallimg[0].onclick=function()
-    {
-        ProductImg.src=smallimg[0].src;
-    }
-    smallimg[1].onclick = function()
-    {
-        ProductImg.src=smallimg[1].src;
-    }
-    smallimg[2].onclick=function()
-    {
-        ProductImg.src=smallimg[2].src;
-    }
-    smallimg[3].onclick=function()
-    {
-        ProductImg.src=smallimg[3].src;
-    }
-
     // for account form
 
 function register() {
